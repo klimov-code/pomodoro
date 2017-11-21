6d7f68fe71fec7926de5398aa1bc9d366cd0ef18
Therapy <therapyspb@yandex.ru>
2017-11-22 00:12:06 +0300

    initial logic, fix index extension, update .gitignore

--- src/Pomodoro.jsx
+++ src/Pomodoro.jsx
@@ -4,10 +4,63 @@ import Timer from './components/Timer';
 import Footer from './components/Footer';
 
 export default class Pomodoro extends Component {
-  state = {
-    play: true,
-    time: 0,
-    break: 0,
-    title: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      play: false,
+      time: 1500,
+      break: 300,
+      title: '',
+      pomodoroCount: 0,
+      breakCount: 0
+    }
+    this.toggle = this.toggle.bind(this);
+  }
+
+  tick() {
+    this.setState(prevState => ({
+      time: prevState.time - 1
+    }))
+  }
+
+  start() {
+    clearInterval(this.countdown);
+    this.countdown = setInterval(
+      () => this.tick(),
+      1000
+    );
+    this.setState({
+      play: true
+    });
+  }
+
+  pause() {
+    clearInterval(this.countdown);
+    this.setState({
+      play: false
+    });
+  }
+
+  toggle() {
+    if (this.state.play) {
+      this.pause();
+    } else {
+      this.start();
+    }
+  }
+
+  formatTime(time) {
+    const format = (number) => (number > 9) ? number : '0' + number;
+
+    return `${format(Math.floor(time / 60 % 60))}:${format(Math.floor(time % 60))}`;
+  }
+
+  render() {
+    return(
+      <div>
+        <div>{this.formatTime(this.state.time)}</div>
+        <button type='button' onClick={this.toggle}>start</button>
+      </div>
+    );
   }
 }
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,16 +2,8 @@ import React, { Component } from 'react';
 
 export default class Timer extends Component {
 
-
-  render() {
-    return(
-
-    );
-  }
 }
 
 class Controls extends Component {
-  render() {
 
-  }
 }
