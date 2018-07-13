c7300374f779ad4ea87a8f1e4e752f1ff3265e37
Therapy <therapyspb@yandex.ru>
2018-07-13 15:36:25 +0300

    final commit before full rewrite

--- src/components/Menu/Header.jsx
+++ src/components/Menu/Header.jsx
@@ -7,6 +7,6 @@ const Header = () =>
       <p>The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. The technique uses a timer to break down work into intervals, traditionally 25 minutes in length, separated by short breaks. These intervals are named pomodoros, the plural in English of the Italian word pomodoro (tomato), after the tomato-shaped kitchen timer that Cirillo used as a university student.</p>
       <p>The technique has been widely popularized by dozens of apps and websites providing timers and instructions. Closely related to concepts such as timeboxing and iterative and incremental development used in software design, the method has been adopted in pair programming contexts.<cite> - Wikipedia</cite></p>
     </blockquote>
-   </header>;
+  </header>;
 
 export default Header;
